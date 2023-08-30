9931457df1272fe25c77010ab76f1cdd0b7103fc
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-08-30 18:39:54 +0500

    serrvice components added to Foode app

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,9 @@
-import { Button, Stack, Text } from '@chakra-ui/react'
+import { Button, Image, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Services = () => {
   return (
-    <Stack display={"flex"} flexDirection={"row"} py={2} my={"3em"}>
-
+    <Stack display={"flex"} flexDirection={"row"} gap={3} py={3} my={"4em"} >
         <div className='services-left'>
             <Text textTransform={"uppercase"} fontSize={"xl"} color={"red.600"} >our story and services</Text>
             <Text
@@ -15,9 +14,43 @@ const Services = () => {
             <Text fontSize={"md"}>
                 Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.
             </Text>
-            <Button borderRadius={"40px"} bg='#39DB4A' color={"white"} >Explore</Button>
+            <Button borderRadius={"40px"} mt={3} bg='#39DB4A' color={"white"} >Explore</Button>
         </div>
+        <div className="services-right">
+            <div className="service">
+                <Image src={require("../assets/fi-rr-gift.png")} alt="image"/>
+                <Text textTransform={"uppercase"} fontWeight={"bold"} fontSize={"xl"} color={"#39DB4A"} >catering</Text>
+            
+                <Text textTransform={"capitalize"} fontSize={"md"} color={"#39DB4A"} >
+                    Delight your guests with our flavors and  presentation
+                </Text>
+            </div>
+            <div className="service">
+                <Image src={require("../assets/fi-rr-salad.png")} alt="image"/>
+                <Text textTransform={"uppercase"} fontWeight={"bold"} fontSize={"xl"} color={"#39DB4A"} >gift cards</Text>
+            
+                <Text textTransform={"capitalize"}  fontSize={"md"} color={"#39DB4A"} >
+                    Delight your guests with our flavors and  presentation
+                </Text>
+            </div>
+            <div className="service">
+                <Image  src={require("../assets/fi-rr-time-fast.png")} alt="image"/>
+                <Text textTransform={"uppercase"} fontWeight={"bold"} fontSize={"xl"} color={"#39DB4A"} >fast delivery</Text>
+            
+                <Text textTransform={"capitalize"} fontSize={"md"} color={"#39DB4A"} >
+                    Delight your guests with our flavors and  presentation
+                </Text>
+            </div>
+            <div className="service">
+                <Image  src={require("../assets/fi-rr-shopping-cart-check.png")} alt="image"/>
+                <Text textTransform={"uppercase"} fontWeight={"bold"} fontSize={"xl"} color={"#39DB4A"} >online order</Text>
+            
+                <Text textTransform={"capitalize"} fontSize={"md"} color={"#39DB4A"} >
+                    Delight your guests with our flavors and  presentation
+                </Text>
+            </div>
 
+        </div>
     </Stack>
   )
 }
